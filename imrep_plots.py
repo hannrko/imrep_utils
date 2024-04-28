d3671f6ac8866e10e8a437017f06a1b6f44a8a37
Hannah Kockelbergh <h.kockelbergh@liverpool.ac.uk>
2024-04-28 13:00:43 +0100

    added legend to step plots

--- imrep_plots.py
+++ imrep_plots.py
@@ -25,6 +25,7 @@ class DatasetPlotter:
         # initialise colour df
         self.sam_colour = pd.DataFrame(index=self.sam_info.index, columns=[])
         self.sam_colour_dicts = {}
+        self.sam_class_name_dicts = {}
         self.data = data
         # choose resolution
         self.resolution = resolution
@@ -64,7 +65,7 @@ class DatasetPlotter:
         ax.text((u_x[0] + u_x[1])/2, yb + h, annot, ha='center', va='bottom', color='k')
         return ax
 
-    def set_colour(self, sam_info_col, colour_dict=None):
+    def set_colour(self, sam_info_col, colour_dict=None, neat_colour_label_dict=None):
         si = self.sam_info[sam_info_col]
         # if no colour specified
         if colour_dict is None:
@@ -75,6 +76,8 @@ class DatasetPlotter:
         self.sam_colour_dicts[sam_info_col] = colour_dict
         colours = [colour_dict[si_i] for si_i in si]
         self.sam_colour[sam_info_col] = colours
+        if neat_colour_label_dict is not None:
+            self.sam_class_name_dicts[sam_info_col] = neat_colour_label_dict
 
     def reorder_samples(self, new_ord=None, ord_func=None):
         if new_ord is None and ord_func is None:
@@ -84,43 +87,73 @@ class DatasetPlotter:
         self.sam_info = self.sam_info.loc[new_ord]
         self.data = self.data[new_ord]
         self.sam_colour = self.sam_colour.loc[new_ord]
+
 class DepthDatasetPlotter(DatasetPlotter):
-    def hist(self, nbins=10 ,xlog=False, ylog=False, title=None, fig_kwargs=None):
+    def hist(self, nbins=10, colour="k", xlog=False, ylog=False, title=None, fig_kwargs=None):
         fig_kwargs = self._empty_kwargs(fig_kwargs)
         fig, ax = plt.subplots(1, 1, **fig_kwargs)
         if xlog:
             ax.set_xscale("log")
-            m = np.amax(self.data.values)
-            ord = np.log10(m)
-            mord = math.ceil(ord)
-            bins = np.logspace(0, stop=mord, num=nbins)#10 ** (np.arange(0, 8))
+            mx = math.ceil(np.log10(np.amax(self.data.values)))
+            mn = math.floor(np.log10(np.amin(self.data.values)))
+            bins = np.logspace(start=mn, stop=mx, num=nbins)
         else:
             bins = nbins
-        ax.hist(self.data, log=ylog, bins=bins)
+        ax.hist(self.data, log=ylog, bins=bins, color=colour)
+        ax.set_xlabel("Counts")
+        ax.set_ylabel("Frequency")
         df_title = "count histogram"
         self._handle_output(fig, df_title, title)
 
-    def samples_dwn(self, colour_name, fig_kwargs=None, title=None):
+    def samples_dwn(self, colour_name, logx=True, dwn_thresh=None, dwn_c="k", fig_kwargs=None, title=None):
         fig_kwargs = self._empty_kwargs(fig_kwargs)
         fig, ax = plt.subplots(1, 1, **fig_kwargs)
-        print(self.data)
         ds = self.data[self.data.columns[0]]
         x = ds.sort_values(ascending=True).values
         cls = np.unique(self.sam_info[colour_name])
-        #clss = [ for cls in ]
         bl = np.zeros(len(x))
         for c in cls:
-            print(c, bl)
             single_cls = ds[self.sam_info[colour_name]==c]
-            y = np.array([sum(single_cls >= count) for count in x]) + bl
-            ax.stairs(y, np.concatenate(([0],x)), fill=True, baseline=bl, color=self.sam_colour_dicts[colour_name][c])
+            y = np.array([sum(single_cls >= count) for count in x])
+            ax.stairs(y+bl, np.concatenate(([0],x)), fill=True, baseline=bl,
+                      color=self.sam_colour_dicts[colour_name][c], label=self.sam_class_name_dicts[colour_name][c])
             bl = bl + y
-        ax.set_xscale("log")
+        if logx:
+            ax.set_xscale("log")
         ax.margins(x=0)
-        ax.axhline(y=len(x))
-
+        ax.axhline(y=len(x), color="k")
+        ax.legend()
+        if dwn_thresh is not None:
+            ax.axvline(x=dwn_thresh, color=dwn_c)
+            # this doesn't work when the threshold comes from a different dataset!
+            dwn_samples = sum(x >= dwn_thresh)
+            ax.axhline(y=dwn_samples, color=dwn_c)
+            ax.scatter(dwn_thresh, dwn_samples, marker="x", color="k")
+            ax.annotate(f"({dwn_thresh}, {dwn_samples})", xy=(dwn_thresh, dwn_samples),
+                        xytext=(50, 50), textcoords='offset pixels')
+        ax.set_yticks(list(ax.get_yticks()) + [len(x)])
+        ax.set_ylim((0, len(x)))
+        ax.set_xlabel("Counts")
+        ax.set_ylabel("Samples")
         df_title = "count bar downsampling"
         self._handle_output(fig, df_title, title)
+
+    def table(self, cls_name, dwn_thresh):
+        ds = self.data[self.data.columns[0]]
+        classes = np.unique(self.sam_info[cls_name])
+        ds_cls = [ds[self.sam_info[cls_name]==cls] for cls in classes]
+        all_ds = [ds, *ds_cls]
+        mean = [np.mean(dst) for dst in all_ds]
+        median = [np.median(dst) for dst in all_ds]
+        count = [len(dst) for dst in all_ds]
+        ds_count = [len(dst[dst >= dwn_thresh]) for dst in all_ds]
+        tab = pd.DataFrame(data=np.column_stack((mean, median, count, ds_count)),
+                           columns=["Mean counts", "Median counts", "Samples", "Samples after downsampling"],
+                           index=["Total", *[self.sam_class_name_dicts[cls_name][cls] for cls in classes]])
+        with open(os.path.join(self.sv_path, "count_tab.tex"), "w") as f:
+            f.write(tab.to_latex())
+
+
 
 
 class DiversityDatasetPlotter(DatasetPlotter):
@@ -137,7 +170,7 @@ class DiversityDatasetPlotter(DatasetPlotter):
         self._handle_output(fig, df_title, title)
         # could split this up into single div plot, then multiple
 
-    def box(self, div_names, colour_name, title=None, annot=None, fig_kwargs=None):
+    def box(self, div_names, colour_name, neat_div_names=None, title=None, annot=None, fig_kwargs=None):
         # use seaborn, tidy data
         # need to combine sam info and data
         info_data = pd.concat([self.data.T, self.sam_info], axis=1, join="inner")
@@ -146,18 +179,24 @@ class DiversityDatasetPlotter(DatasetPlotter):
             div_names = [div_names]
         if annot is None:
             annot = [None]*len(div_names)
-        fig, axs = plt.subplots(1, len(div_names), **fig_kwargs)
+        fig, axs = plt.subplots(1, len(div_names),  **fig_kwargs)
         if len(div_names) == 1:
             axs = [axs]
         for i, div_name in enumerate(div_names):
             axs[i] = self._box(info_data, colour_name, div_name, axs[i], annot[i])
+            if neat_div_names is not None:
+                axs[i].set_ylabel(neat_div_names[i])
+            axs[i].set_xticklabels([self.sam_class_name_dicts[colour_name][lab] for lab in list(axs[i].get_xticks())])
+            axs[i].set_xlabel("")
+        diy_leg = [mpl.patches.Patch(color=c, label=self.sam_class_name_dicts[colour_name][ln]) for ln, c in self.sam_colour_dicts[colour_name].items()]
+        fig.legend(handles=diy_leg)
         fig.set_tight_layout(True)
         df_title = " ".join(div_names) + " boxplot"
         self._handle_output(fig, df_title, title)
 
     def _box(self, info_data, colour_name, div_name, ax, annot):
         ax = sns.boxplot(data=info_data, x=colour_name, y=div_name, hue=colour_name,
-                         palette=self.sam_colour_dicts[colour_name], ax=ax)
+                         palette=self.sam_colour_dicts[colour_name], legend=False, ax=ax)
         if annot is not None:
             ax = self._annot_box(annot, info_data[colour_name], info_data[div_name], ax)
         return ax
@@ -182,8 +221,8 @@ class VDJDatasetPlotter(DatasetPlotter):
         if norm:
             self.data = self.data/self.data.sum()#, axis="index")#/np.array(list(vdj_data.sum(axis=1).values))
         seg_ord = self.data.sum(axis=1).sort_values(ascending=False)
-        print(self.data)
         self.data = self.data.loc[seg_ord.index]
+
     def heatmap(self, colour_name, type, annots=None, cmap="binary", vmax=None, disp_cbar=True, title=None, fig_kwargs=None):
         fig_kwargs = self._empty_kwargs(fig_kwargs)
         fig, ax = plt.subplots(1, 1, **fig_kwargs)
@@ -208,13 +247,18 @@ class VDJDatasetPlotter(DatasetPlotter):
         info_data = vdj.merge(ri_sam_info, left_on="sample", right_on="sample")
         fig_kwargs = self._empty_kwargs(fig_kwargs)
         fig, ax = plt.subplots(1, 1, **fig_kwargs)
-        ax = sns.boxplot(data=info_data, x="seg", y="count", hue=colour_name,
+        ax = sns.boxplot(data=info_data, x="seg", y="count", hue=colour_name, legend=False,
                          palette=self.sam_colour_dicts[colour_name], ax=ax)
         if annots is not None:
             for i, annot in enumerate(annots):
                 seg_data = info_data[info_data["seg"] == info_data["seg"].iloc[i]]
                 ax = self._annot_box(annot, i+seg_data[colour_name]-1/2, seg_data["count"], ax)
+        diy_leg = [mpl.patches.Patch(color=c, label=self.sam_class_name_dicts[colour_name][ln]) for ln, c in
+                   self.sam_colour_dicts[colour_name].items()]
+        fig.legend(handles=diy_leg)
         ax.set_xticks(ax.get_xticks(), ax.get_xticklabels(), rotation=90)
+        ax.set_ylabel("Proportion of usage within repertoire")
+        ax.set_xlabel(f"{type} gene segment")
         fig.set_tight_layout(True)
         df_title = f"{type} segment boxplot"
         self._handle_output(fig, df_title, title)
